069ee3d8b6fe9fe53515104f13e1982fe3e343f3
MasonLee219  definitelymason@gmail.com <definitelymason@gmail.com>
2019-09-30 19:03:12 +0100

    Pushing intial progress

---
+++ TextBasedMissileGame/TextBasedMissileGame.h
@@ -0,0 +1,73 @@
+#include "stdafx.h"
+#include <iostream>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+int const BATTLEFIELD_MAX_SIZE_X = 5;
+int const BATTLEFIELD_MAX_SIZE_Y = 5;
+int const BATTLEFIELD_MIN_SIZE_X = 1;
+int const BATTLEFIELD_MIN_SIZE_Y = 1;
+int const ENEMY_COUNT = 5;
+int const LAUNCH_CODE = 1234;
+
+enum Warhead { Ballistic, Nuclear };
+
+bool isArmed = false;
+bool isRunning = false;
+
+typedef struct Position
+{	
+	int x;
+	int y;
+	vector<vector<pair<int, int>>> coordinates;
+
+	void print()
+	{
+		cout << x << y << endl;
+	}
+}Coordinates;
+
+struct Userinputs
+{
+	Coordinates coordinates;
+	int userMissileChoice;
+	int userLaunchCode;
+};
+
+typedef struct Enemy
+{
+	Coordinates coordinates;
+}Target;
+
+
+struct Missile
+{
+	Warhead payload;
+	Coordinates coordinates;
+
+	Target targets[ENEMY_COUNT];
+
+	bool armed;
+
+	void arm()
+	{
+		if (isArmed)
+			isArmed = false;
+		else
+			isArmed = true;
+	}
+
+	void update()
+	{
+		coordinates.x += 1;
+		coordinates.y += 2;
+	}
+};
+
+
+
+
+
+
